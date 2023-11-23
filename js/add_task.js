fee20895d72f9e05642ee285c36280330ccd4c28
DustinRs <drohrschneider@web.de>
2023-11-23 11:51:52 +0100

    addtaskpage

---
+++ js/add_task.js
@@ -0,0 +1,33 @@
+let allTasks = [];
+
+function addTask() {
+    let title = document.getElementById('title').value;
+    let description = document.getElementById('description').value;
+    let date = document.getElementById('date').value;
+    let prio = document.getElementById('urgent').value;
+    let category = document.getElementById('category').value;
+    let subTask = document.getElementById('subTask').value;
+
+
+    let task = {
+        'title': title,
+        'description': description,
+        'date': date,
+        'prio': prio,
+        'category': category,
+        'subTask': subTask
+    };
+console.log(task);
+    allTasks.push(task);
+
+    
+
+    let allTasksAsString = JSON.stringify(allTasks);
+    localStorage.setItem('allTasks', allTasksAsString);
+    
+}
+
+function loadAllTasks() {
+    let allTasksAsString = localStorage.getItem('allTasks');
+    allTasks = JSON.parse(allTasksAsString);
+}
