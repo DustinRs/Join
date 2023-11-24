87b611b6b0b35cdbb9cca00d53d43c01e70a148f
DustinRs <drohrschneider@web.de>
2023-11-24 19:03:19 +0100

    designed add_task and added functions

--- js/add_task.js
+++ js/add_task.js
@@ -1,52 +1,128 @@
 let allTasks = [];
-const STORAGE_TOKEN = 'QFOSCYPA967P352YSSOENCUXGKA464XWSUTNI5NT';
-const STORAGE_URL = 'https://remote-storage.developerakademie.org/item';
+const STORAGE_TOKEN = "QFOSCYPA967P352YSSOENCUXGKA464XWSUTNI5NT";
+const STORAGE_URL = "https://remote-storage.developerakademie.org/item";
+let prioArray = [];
 
 function addTask() {
-    let title = document.getElementById('title').value;
-    let description = document.getElementById('description').value;
-    let date = document.getElementById('date').value;
-    let prio = document.getElementById('urgent').value;
-    let category = document.getElementById('category').value;
-    let subTask = document.getElementById('subTask').value;
-    let task = {
-        'title': title,
-        'description': description,
-        'date': date,
-        'prio': prio,
-        'category': category,
-        'subTask': subTask
-    };
-    console.log(task);
-    allTasks.push(task);
+  let title = document.getElementById("title").value;
+  let description = document.getElementById("description").value;
+  let date = document.getElementById("date").value;
+  let prio = prioArray;
+  let category = document.getElementById("category").value;
+  let subTask = document.getElementById("subTask").value;
+  let task = {
+    title: title,
+    description: description,
+    date: date,
+    prio: prio,
+    category: category,
+    subTask: subTask,
+  };
+  console.log(task);
+  allTasks.push(task);
 
-
-
-    let allTasksAsString = JSON.stringify(allTasks);
-    localStorage.setItem('allTasks', allTasksAsString);
-
+  let allTasksAsString = JSON.stringify(allTasks);
+  localStorage.setItem("allTasks", allTasksAsString);
 }
 
 function loadAllTasks() {
-    let allTasksAsString = localStorage.getItem('allTasks');
-    allTasks = JSON.parse(allTasksAsString) || [];
-}
-
-function checkBoxClicked(id) {
-    let checkbox = document.getElementById(id);
-    if (checkbox.checked) {
-        console.log(checkbox.value);
-        
-    }
+  let allTasksAsString = localStorage.getItem("allTasks");
+  allTasks = JSON.parse(allTasksAsString) || [];
 }
 
 async function setItem(key, value) {
-    const payload = { key, value, token: STORAGE_TOKEN };
-    return fetch(STORAGE_URL, { method: 'POST', body: JSON.stringify(payload)})
-    .then(res => res.json());
+  const payload = { key, value, token: STORAGE_TOKEN };
+  return fetch(STORAGE_URL, {
+    method: "POST",
+    body: JSON.stringify(payload),
+  }).then((res) => res.json());
 }
 
 async function getItem(key) {
-    const url = `${STORAGE_URL}?key=${key}&token=${STORAGE_TOKEN}`;
-    return fetch(url).then(res => res.json());
+  const url = `${STORAGE_URL}?key=${key}&token=${STORAGE_TOKEN}`;
+  return fetch(url).then((res) => res.json());
+}
+
+function checkBoxClicked(priority) {
+  let checkbox = document.getElementById(priority);
+  let image = checkbox.closest(".prio").querySelector(".prioImgs");
+
+  if (checkbox.checked) {
+    deactivateOtherCheckboxes(priority);
+    checkbox.parentNode.parentNode.style.backgroundColor = getColor(priority);
+    changeImageSrc(priority, image);
+  } else {
+    checkbox.parentNode.parentNode.style.backgroundColor = "";
+    image.src = "/assets/img/" + priority.toLowerCase() + "-priority.png";
+  }
+}
+
+function deactivateOtherCheckboxes(currentPriority) {
+  const priorities = ["urgent", "medium", "low"];
+  for (const priority of priorities) {
+    if (priority !== currentPriority) {
+      document.getElementById(priority).checked = false;
+      document.getElementById(
+        priority
+      ).parentNode.parentNode.style.backgroundColor = "";
+      document
+        .getElementById(priority)
+        .closest(".prio")
+        .querySelector(".prioImgs").src =
+        "/assets/img/" + priority.toLowerCase() + "-priority.png";
+    }
+  }
+  pushCurrentPriority(currentPriority);
+}
+
+function pushCurrentPriority(currentPriority) {
+  if (prioArray.length > 0) {
+    prioArray[0] = currentPriority;
+  } else {
+    prioArray.push(currentPriority);
+  }
+}
+
+function getColor(priority) {
+  switch (priority) {
+    case "urgent":
+      return "#FF3D00";
+    case "medium":
+      return "#FFA800";
+    case "low":
+      return "#7AE229";
+    default:
+      return "";
+  }
+}
+
+function changeImageSrc(priority, image) {
+  let basePath = "/assets/img/";
+  let activeFileName = priority.toLowerCase() + "-active.png";
+  let newSrc = basePath + activeFileName;
+
+  checkImageExists(newSrc, function (exists) {
+    if (exists) {
+      if (image instanceof HTMLImageElement) {
+        image.onload = function () {};
+
+        image.onerror = function () {};
+
+        image.src = newSrc;
+      } else {
+      }
+    } else {
+    }
+  });
+}
+
+function checkImageExists(url, callback) {
+  let img = new Image();
+  img.onload = function () {
+    callback(true);
+  };
+  img.onerror = function () {
+    callback(false);
+  };
+  img.src = url;
 }
