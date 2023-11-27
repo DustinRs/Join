b457a85620345e4f8d69d582430457797efc28e9
DustinRs <drohrschneider@web.de>
2023-11-27 19:08:10 +0100

    contact list renders perfectly

--- js/add_contact.js
+++ js/add_contact.js
@@ -21,6 +21,30 @@ let contacts = [
     email: "test@123.de",
     phoneNumber: "01234567",
   },
+  {
+    firstName: "Albert",
+    name: " Müller",
+    email: "test@123.de",
+    phoneNumber: "01234567",
+  },
+  {
+    firstName: "Gerd",
+    name: " Müller",
+    email: "test@123.de",
+    phoneNumber: "01234567",
+  },
+  {
+    firstName: "Thomas",
+    name: " Müller",
+    email: "test@123.de",
+    phoneNumber: "01234567",
+  },
+  {
+    firstName: "Herbert",
+    name: " Müller",
+    email: "test@123.de",
+    phoneNumber: "01234567",
+  },
 ];
 
 let letters = [
@@ -55,6 +79,7 @@ let letters = [
 async function init() {
   getItem(key);
   renderRegister();
+  hideUnusedLetters();
 }
 
 function renderRegister() {
@@ -72,7 +97,7 @@ function renderRegister() {
     <div class="registerLine">
     </div>
     </div>
-    <div id="${letter}">
+    <div id="${letter}" class="test">
 
     </div>`;
 
@@ -89,20 +114,39 @@ function renderContacts(letter) {
     let fullName = contact["firstName"] + " " + contact["name"];
     let initials =
       contacts[i].firstName.slice(0, 1) + contacts[i].name.slice(0, 1);
-    container.innerHTML += `
+    let initial = contacts[i].firstName.slice(0, 1);
+
+    if (letter === initial) {
+      container.innerHTML += `
         <div id="${fullName}" onclick="openProfile('${fullName}', '${contact["email"]}', '${contact["phoneNumber"]}')" class="contact">
         
         <div>
             <span id="${initials}" class="initials">${initials}</span>
         </div>
         <div class="nameLinkDiv">
-            <span>${fullName}</span>
+            <span class="fullName">${fullName}</span>
             <a class="emailLinks" href="#">${contact["email"]}</a>
         </div>
     </div>`;
-    getRandomColor(`${initials}`);
+      getRandomColor(`${initials}`);
+    }
   }
-  
+}
+
+function hideUnusedLetters() {
+  let divElements = document.querySelectorAll(".test");
+  let divRegister = document.querySelectorAll(".register");
+  let divLine = document.querySelectorAll(".registerLineDiv");
+
+  divElements.forEach((element, index) => {
+    if (element.innerHTML.trim() === "") {
+      divRegister[index].classList.add("d-none");
+      divLine[index].classList.add("d-none");
+    } else {
+      divRegister[index].classList.remove("d-none");
+      divLine[index].classList.remove("d-none");
+    }
+  });
 }
 
 function openProfile(name, mail, number) {
@@ -146,20 +190,20 @@ function setContactBackgroundColor(name) {
 }
 
 function openPopUpAddContact() {
-  document.getElementById('addContactPopUp').classList.remove('d-none');
+  document.getElementById("addContactPopUp").classList.remove("d-none");
 }
 
 function closePopUpAddContact() {
-  document.getElementById('addContactPopUp').classList.add('d-none');
+  document.getElementById("addContactPopUp").classList.add("d-none");
 }
 
 async function createContact() {
-  let fullName = document.getElementById('profileName').value;
-  let email = document.getElementById('profileEmail').value;
-  let number = document.getElementById('profileNumber').value;
+  let fullName = document.getElementById("profileName").value;
+  let email = document.getElementById("profileEmail").value;
+  let number = document.getElementById("profileNumber").value;
 
-  let firstName = fullName.split(' ').slice(0, -1).join(' ');
-  let name = fullName.split(' ').slice(-1).join(' ');
+  let firstName = fullName.split(" ").slice(0, -1).join(" ");
+  let name = fullName.split(" ").slice(-1).join(" ");
 
   let contact = {
     firstName: firstName,
@@ -171,7 +215,6 @@ async function createContact() {
   contacts.push(contact);
   setItem(key, contacts);
   init();
-
 }
 
 async function setItem(key, value) {
@@ -187,9 +230,18 @@ async function getItem(key) {
   return fetch(url).then((res) => res.json());
 }
 
-
 function randomColor() {
-  let colors = ["#FF7A00", "#9327FF", "#6E52FF", "#FC71FF", "#FFBB2B", "#1FD7C1",'#462F8A', '#FF4646', '#00BEE8'];
+  let colors = [
+    "#FF7A00",
+    "#9327FF",
+    "#6E52FF",
+    "#FC71FF",
+    "#FFBB2B",
+    "#1FD7C1",
+    "#462F8A",
+    "#FF4646",
+    "#00BEE8",
+  ];
   let randomIndex = Math.floor(Math.random() * colors.length);
   return colors[randomIndex];
 }
