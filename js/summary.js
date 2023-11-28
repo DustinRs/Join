95416c824df65be28f7eea3448162288ced855ae
Rene Heller <128136936+L0ki7778@users.noreply.github.com>
2023-11-28 23:18:12 +0100

    commit

--- js/summary.js
+++ js/summary.js
@@ -1,35 +1,92 @@
 const STORAGE_TOKEN = "QFOSCYPA967P352YSSOENCUXGKA464XWSUTNI5NT";
 const STORAGE_URL = "https://remote-storage.developerakademie.org/item";
 
-let allTasks = [];
+let allTasks = [
+  // {
+  //   title: "apfel",
+  //   description: "description",
+  //   date: "date",
+  //   prio: "low",
+  //   category: "in progress",
+  //   subTask: "subTask",
+  // },
+  // {
+  //   title: "banana",
+  //   description: "description",
+  //   date: "date",
+  //   prio: "low",
+  //   category: "toDo",
+  //   subTask: "subTask",
+  // },
+  // {
+  //   title: "citrus",
+  //   description: "description",
+  //   date: "date",
+  //   prio: "medium",
+  //   category: "await feedback",
+  //   subTask: "subTask",
+  // },
+  // {
+  //   title: "dattel",
+  //   description: "description",
+  //   date: "date",
+  //   prio: "urgent",
+  //   category: "done",
+  //   subTask: "subTask",
+  // },
+  // {
+  //   title: "eimer",
+  //   description: "description",
+  //   date: "date",
+  //   prio: "prio",
+  //   category: "done",
+  //   subTask: "subTask",
+  // },
+  // {
+  //   title: "fussball",
+  //   description: "description",
+  //   date: "date",
+  //   prio: "urgent",
+  //   category: "done",
+  //   subTask: "subTask",
+  // }
+];
 let key = "notes";
 
 function init() {
   renderNotes();
 }
 
-/*
+function todoFilter(){
+  let arr = allTasks.filter((e)=>e["category"] == "done");
+  console.log(arr)
+}
+
 function renderNotes() {
     let container = document.querySelector('main');
     
     container.innerHTML = `<div class="grid-container1">
-    <div id="To-Do" class="grid-item">${toDo.length}</div>
-    <div id="Done" class="grid-item">${done.length}</div>
+    <div id="To-Do" class="grid-item"></div>
+    <div id="Done" class="grid-item"></div>
   </div>
   <div class="grid-container2">
-    <div id="urgent" class="grid-item">${urgent.length}</div>
+    <div id="urgent" class="grid-item"></div>
   </div>
   <div class="grid-container3">
-    <div id="taskInBoard" class="grid-item">${tasksInBoard.length}</div>
-    <div id="taskInProgress" class="grid-item">${inProgress.length}</div>
-    <div id="awaitingFeedback" class="grid-item">${awaitingFeedback.length}</div>
+    <div id="taskInBoard" class="grid-item"></div>
+    <div id="taskInProgress" class="grid-item"></div>
+    <div id="awaitingFeedback" class="grid-item"></div>
   </div>`;
 
 }
-*/
+
 async function getItem(key) {
   const url = `${STORAGE_URL}?key=${key}&token=${STORAGE_TOKEN}`;
-  return fetch(url).then((res) => res.json());
+ fetch(url)
+ .then((res) => res.json())
+ .then(json=>{
+  return allTasks=json.data.value;
+ });
 }
 
 async function setItem(key, value) {
