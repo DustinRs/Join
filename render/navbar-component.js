f33267be140238836c2afd9cc2c8ee39385b3933
DustinRs <drohrschneider@web.de>
2023-11-30 04:33:12 +0100

    navbar-component rdy

--- render/navbar-component.js
+++ render/navbar-component.js
@@ -1,24 +1,24 @@
-function renderNavBar(){
-    return/*html*/`
+function renderNavBar() {
+  return /*html*/ `
     <div class="navBar">
         <img class="navLogo" src="/assets/img/NavLogo.png" alt="">
         <div class="navLinks">
-        <div class="navLinkImg">
-          <a href="/assets/templates/summary.html">
+        
+          <a class="navLinkImg" href="/assets/templates/summary.html">
           <img class="linkImg" src="/assets/img/nav-summary.png" alt=""><span>Summary</span></a>
-        </div>
-        <div class="navLinkImg"> 
-          <a href="/assets/templates/add_task.html">
+        
+         
+          <a class="navLinkImg" href="/assets/templates/add_task.html">
           <img class="linkImg" src="/assets/img/nav-add-task.png" alt=""><span>Add Task</span></a>
-        </div>
-        <div class="navLinkImg">
-          <a href="/assets/templates/board.html">
+        
+        
+          <a class="navLinkImg" href="/assets/templates/board.html">
           <img class="linkImg" src="/assets/img/nav-board.png" alt=""><span>Board</span></a>
-        </div>
-        <div class="navLinkImg">
-          <a href="/assets/templates/contacts.html">
+        
+        
+          <a class="navLinkImg" href="/assets/templates/contacts.html">
           <img class="linkImg" src="/assets/img/nav-contacts.png" alt=""><span>Contacts</span></a>
-        </div>
+        
         </div>
         <div class="navBottom">
             <a class="navLinksBot" href="/assets/templates/privacy_policy.html">Privacy Policy</a>
@@ -27,5 +27,5 @@ function renderNavBar(){
     <section>
         
     </section>
-    `
+    `;
 }
