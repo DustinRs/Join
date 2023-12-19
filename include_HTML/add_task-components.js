0c48b1d3a4c0048aa154b7c66ae85e80a842616b
DustinRs <drohrschneider@web.de>
2023-12-19 10:17:25 +0100

    commit

--- include_HTML/add_task-components.js
+++ include_HTML/add_task-components.js
@@ -168,11 +168,11 @@ function renderSubTasksList() {
     let list = document.getElementById('task-list');
     list.innerHTML = '';
     subTasks.forEach((e) => {
-        list.innerHTML += `<li class="single-subtask" id="${subTasks.indexOf(e)}">${e}</li>`
+        list.innerHTML += `<li class="single-subtask" onclick="editListItem(${subTasks.indexOf(e)})" id="${subTasks.indexOf(e)}">${e}</li>`
     })
     if (finishedSubTasks.length > 0) {
         finishedSubTasks.forEach((e) => {
-            list.innerHTML += `<li class="single-finished-task" id="f${finishedSubTasks.indexOf(e)}">${e}</li>`
+            list.innerHTML += `<li class="single-finished-task" onclick="editListItem(${subTasks.indexOf(e)}) id="f${finishedSubTasks.indexOf(e)}">${e}</li>`
         })
     }
 }
